f4108844557adbc5ecc72f45eafb557081f1c0cc
Igor <GIV_84@mail.ru>
2023-12-05 12:13:53 +0400

    add

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,11 +1,19 @@
 from django import forms
 from .models import Task
+from django.utils.translation import gettext as _
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["name", "description", "status", "executor", "labels"]
+        labels = {
+            'name': _('Name'),
+            'description': _('Description'),
+            'status': _('status'),
+            'executor': _('Executor'),
+            'labels': _('Labels'),
+        }
 
 
 
