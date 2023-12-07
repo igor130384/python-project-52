207bb55dc315a05f6b5dfee26ed113a1065e1a80
Igor <GIV_84@mail.ru>
2023-12-07 10:21:39 +0400

    add

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -112,12 +112,12 @@ DATABASES = {
         conn_max_age=600
     )
 }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 AUTH_USER_MODEL = 'users.User'
