7a2f16617b964a5e63a982716640fdbce606ec5c
Igor <GIV_84@mail.ru>
2023-12-07 10:11:14 +0400

    add

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -107,8 +107,8 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default=os.getenv('DATABASE_URL'),
-        # default='sqlite:///db.sqlite3',
+        # default=os.getenv('DATABASE_URL'),
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
